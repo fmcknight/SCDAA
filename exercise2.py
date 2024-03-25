40058b801345adecc4fbda91d424362922355cbb
ananyajai <73023190+ananyajai@users.noreply.github.com>
2024-03-25 14:24:47 +0000

    Tidied up code for exercise 2

---
+++ exercise2.py
@@ -0,0 +1,112 @@
+import numpy as np
+from numpy.linalg import inv
+from scipy.integrate import odeint, solve_ivp
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+# needed for exercise 2.1
+class DGM_Layer(nn.Module):
+    
+    def __init__(self, dim_x, dim_S, activation='Tanh'):
+        super(DGM_Layer, self).__init__()
+        
+        if activation == 'ReLU':
+            self.activation = nn.ReLU()
+        elif activation == 'Tanh':
+            self.activation = nn.Tanh()
+        elif activation == 'Sigmoid':
+            self.activation = nn.Sigmoid()
+        elif activation == 'LogSigmoid':
+            self.activation = nn.LogSigmoid()
+        else:
+            raise ValueError("Unknown activation function {}".format(activation))
+            
+
+        self.gate_Z = self.layer(dim_x+dim_S, dim_S)
+        self.gate_G = self.layer(dim_x+dim_S, dim_S)
+        self.gate_R = self.layer(dim_x+dim_S, dim_S)
+        self.gate_H = self.layer(dim_x+dim_S, dim_S)
+            
+    def layer(self, nIn, nOut):
+        l = nn.Sequential(nn.Linear(nIn, nOut), self.activation)
+        return l
+    
+    def forward(self, x, S):
+        x_S = torch.cat([x,S],1)
+        Z = self.gate_Z(x_S)
+        G = self.gate_G(x_S)
+        R = self.gate_R(x_S)
+        
+        input_gate_H = torch.cat([x, S*R],1)
+        H = self.gate_H(input_gate_H)
+        
+        output = ((1-G))*H + Z*S
+        return output
+
+
+class Net_DGM(nn.Module):
+
+    def __init__(self, dim_x, dim_S, hidden_size=100, activation='Tanh'):
+        super(Net_DGM, self).__init__()
+
+        self.dim = dim_x
+        if activation == 'ReLU':
+            self.activation = nn.ReLU()
+        elif activation == 'Tanh':
+            self.activation = nn.Tanh()
+        elif activation == 'Sigmoid':
+            self.activation = nn.Sigmoid()
+        elif activation == 'LogSigmoid':
+            self.activation = nn.LogSigmoid()
+        else:
+            raise ValueError("Unknown activation function {}".format(activation))
+
+        self.input_layer = nn.Sequential(nn.Linear(dim_x+1, dim_S), self.activation)
+
+        self.DGM1 = DGM_Layer(dim_x=dim_x+1, dim_S=dim_S, activation=activation)
+        self.DGM2 = DGM_Layer(dim_x=dim_x+1, dim_S=dim_S, activation=activation)
+        self.DGM3 = DGM_Layer(dim_x=dim_x+1, dim_S=dim_S, activation=activation)
+
+        self.output_layer = nn.Linear(hidden_size, 1)
+
+    def forward(self,t,x):
+        tx = torch.cat([t,x], 1)
+        S1 = self.input_layer(tx)
+        S2 = self.DGM1(tx,S1)
+        S3 = self.DGM2(tx,S2)
+        S4 = self.DGM3(tx,S3)
+        output = self.output_layer(S4)
+        return output
+    
+
+# needed for exercise 2.2
+class FFN(nn.Module):
+
+    def __init__(self, sizes, activation=nn.ReLU, output_activation=nn.Identity, batch_norm=False):
+        super().__init__()
+        
+        layers = [nn.BatchNorm1d(sizes[0]),] if batch_norm else []
+        for j in range(len(sizes)-1):
+            layers.append(nn.Linear(sizes[j], sizes[j+1]))
+            if batch_norm:
+                layers.append(nn.BatchNorm1d(sizes[j+1], affine=True))
+            if j<(len(sizes)-2):
+                layers.append(activation())
+            else:
+                layers.append(output_activation())
+
+        self.net = nn.Sequential(*layers)
+
+    def freeze(self):
+        for p in self.parameters():
+            p.requires_grad=False
+
+    def unfreeze(self):
+        for p in self.parameters():
+            p.requires_grad=True
+
+    def forward(self, x):
+        return self.net(x)
