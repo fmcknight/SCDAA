b46d503bf29a7b426c3315181d62d9995bc3d621
ananyajai <73023190+ananyajai@users.noreply.github.com>
2024-04-07 22:40:22 +0000

    Tiding up code

--- DGM.py
+++ DGM.py
@@ -81,32 +81,3 @@ class Net_DGM(nn.Module):
         output = self.output_layer(S4)
         return output
     
-
-# needed for exercise 2.2
-class FFN(nn.Module):
-
-    def __init__(self, sizes, activation=nn.ReLU, output_activation=nn.Identity, batch_norm=False):
-        super().__init__()
-        
-        layers = [nn.BatchNorm1d(sizes[0]),] if batch_norm else []
-        for j in range(len(sizes)-1):
-            layers.append(nn.Linear(sizes[j], sizes[j+1]))
-            if batch_norm:
-                layers.append(nn.BatchNorm1d(sizes[j+1], affine=True))
-            if j<(len(sizes)-2):
-                layers.append(activation())
-            else:
-                layers.append(output_activation())
-
-        self.net = nn.Sequential(*layers)
-
-    def freeze(self):
-        for p in self.parameters():
-            p.requires_grad=False
-
-    def unfreeze(self):
-        for p in self.parameters():
-            p.requires_grad=True
-
-    def forward(self, x):
-        return self.net(x)
